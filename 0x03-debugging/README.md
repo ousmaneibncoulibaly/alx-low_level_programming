7aa2710981a870c64b07f6983f67fb87a711b5d1
Ousmane Iboun Coulibaly MAIGA <ousmane.ibn.coulibaly@outlook.be>
2023-02-22 18:14:56 +0000

    add debbuging tasking

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+#This is a README about the folder of the third project of C course and deals with the topic of debugging
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * file: main.h
+ * Auth: Maiga Ousmane Iboun Coulibaly
+ * Desc: Header file containing declarations for all functions
+ *       used in the debugging directory.
+ */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */
