4a88b618211d03e75d6c999b74ca7abf9428a679
Ousmane Iboun Couibaly <ousmane.ibn.coulibaly@outlook.be>
2023-08-04 23:55:48 +0000

    add a recursion file that print a string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,1 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/****************** FUNCTIONS ******************/
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
