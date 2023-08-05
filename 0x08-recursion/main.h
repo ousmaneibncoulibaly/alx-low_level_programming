494c68749f5c42a055eba8948a42efaf231f2941
Ousmane Iboun Couibaly <ousmane.ibn.coulibaly@outlook.be>
2023-08-05 04:38:20 +0000

    Add rev recursion file for compilation

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 /****************** FUNCTIONS ******************/
+int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
