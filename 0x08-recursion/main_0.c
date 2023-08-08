3f49b241d948b74a27024f32199f459334ba82f6
Ousmane Iboun Couibaly <ousmane.ibn.coulibaly@outlook.be>
2023-08-08 21:36:37 +0000

    too palindrome check files

---
+++ 0x08-recursion/main_0.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int r;
+
+r = is_palindrome("abcddcba");
+printf("%d\n", r);
+return (0);
+}
---
+++ 0x08-recursion/main_1.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int r;
+r = is_palindrome("abcdcba");
+printf("%d\n", r);
+return (0);
+}
