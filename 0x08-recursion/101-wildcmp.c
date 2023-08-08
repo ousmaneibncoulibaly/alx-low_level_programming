470c6a6b9445101b19044bdd973d762e2b6d32a2
Ousmane Iboun Couibaly <ousmane.ibn.coulibaly@outlook.be>
2023-08-08 22:29:41 +0000

    fix wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,28 +7,35 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '*')
-{
-if (*(s2 + 1) == '\0')
+if (*s1 == '\0' && *s2 == '\0')
 {
 return (1);
 }
-while (*s1 != '\0')
+else if (*s1 == '\0' && *s2 == '*')
 {
-if (wildcmp(s1, s2 + 1))
+return (wildcmp(s1, (s2 + 1)));
+}
+else if (*s1 == '\0' || *s2 == '\0')
 {
-return (1);
-}
-s1++;
-}
+return (0);
 }
 if (*s1 == *s2)
 {
-if (*s1 == '\0')
+return (wildcmp((s1 + 1), (s2 + 1)));
+}
+
+if (*s2 == '*')
+{
+if (*(s2 + 1) == '*')
+{
+return (wildcmp(s1, (s2 + 1)));
+}
+else if (wildcmp(s1, s2 + 1))
 {
 return (1);
 }
-return (wildcmp(s1 + 1, s2 + 1));
+else
+return (wildcmp(s1 + 1, s2));
 }
 return (0);
 }
